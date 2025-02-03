62e58a630cb56bf635888ebab2a2d5cd1ad4d410
amare1990 <amaremek@gmail.com>
2025-02-03 14:25:24 +0300

    Add a method to clean data

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -2,6 +2,8 @@ import os
 import logging
 import pandas as pd
 
+import re
+
 
 class DataProcessor:
     def __init__(self, raw_data_path='../data/scraped_data.csv', cleaned_data_path='../data/cleaned_data.csv', db_path='../data/medical_data.db'):
@@ -31,4 +33,48 @@ class DataProcessor:
             return df
         except Exception as e:
             logging.error(f"Error loading data: {e}")
+            return None
+
+    def clean_data(self, df):
+        """Performs data cleaning tasks: duplicates removal, handling missing values, and format standardization."""
+        if df is None:
+            logging.error("DataFrame is None, skipping cleaning.")
+            return None
+
+        try:
+            # Removing duplicates
+            df.drop_duplicates(inplace=True)
+
+            # Handling missing values (filling with empty string or mean for numeric)
+            for col in df.columns:
+                if df[col].dtype == 'object':
+                    df[col].fillna("", inplace=True)
+                else:
+                    df[col].fillna(df[col].mean(), inplace=True)
+
+            # Remove emojis
+            # emoji_pattern = re.compile(
+            #     "["
+            #     "\U0001F600-\U0001F64F"  # emoticons
+            #     "\U0001F300-\U0001F5FF"  # symbols & pictographs
+            #     "\U0001F680-\U0001F6FF"  # transport & map symbols
+            #     "\U0001F700-\U0001F77F"  # alchemical symbols
+            #     "\U0001F780-\U0001F7FF"  # geometric shapes extended
+            #     "\U0001F800-\U0001F8FF"  # supplemental arrows-c
+            #     "\U0001F900-\U0001F9FF"  # supplemental symbols & pictographs
+            #     "\U0001FA00-\U0001FA6F"  # chess symbols
+            #     "\U0001FA70-\U0001FAFF"  # symbols & pictographs extended-a
+            #     "\U00002702-\U000027B0"  # dingbats
+            #     "\U000024C2-\U0001F251"
+            #     "]+",
+            #     flags=re.UNICODE
+            # )
+
+            # emoji_pattern.sub(r'', df['message'])
+
+
+            logging.info("Data cleaning completed successfully.")
+            return df
+        except Exception as e:
+            logging.error(f"Error during data cleaning: {e}")
             return None
