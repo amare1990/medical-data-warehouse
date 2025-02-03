0b1fb9c4a0cbc12faf07d73ab4afb051e3dc57b6
amare1990 <amaremek@gmail.com>
2025-02-03 13:56:38 +0300

    Create data_cleaning module and DataProcessor class and initialize the class

---
+++ scripts/data_cleaning.py
@@ -0,0 +1,24 @@
+import os
+import logging
+import pandas as pd
+
+
+class DataProcessor:
+    def __init__(self, raw_data_path='../data/scraped_data.csv', cleaned_data_path='../data/cleaned_data.csv', db_path='../data/medical_data.db'):
+        """
+        Initializes the DataProcessor class.
+
+        :param raw_data_path: Path to the raw data file.
+        :param cleaned_data_path: Path to save the cleaned data.
+        :param db_path: Path to SQLite database.
+        """
+        self.raw_data_path = raw_data_path
+        self.cleaned_data_path = cleaned_data_path
+        self.db_path = db_path
+
+        # Ensure data directory exists
+        os.makedirs(os.path.dirname(self.cleaned_data_path), exist_ok=True)
+        os.makedirs(os.path.dirname(self.db_path), exist_ok=True)
+
+        # Set up logging
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
