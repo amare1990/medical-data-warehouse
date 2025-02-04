552075c3515cef9defdf456072c1a168129e5d24
amare1990 <amaremek@gmail.com>
2025-02-04 12:11:00 +0300

    Update the main directory path of the main pipeliner

--- main/main.py
+++ main/main.py
@@ -5,6 +5,16 @@ import nest_asyncio
 # Apply nested asyncio (for Jupyter Notebook use cases)
 nest_asyncio.apply()
 
+import sys
+import os
+
+# Get the root directory of the project
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Add it to sys.path
+sys.path.append(ROOT_DIR)
+
+print(f"Current working directory: {os.getcwd()}")
 from scripts.telegram_scrapping import TelegramScraper
 from scripts.data_cleaning import DataProcessor
 
