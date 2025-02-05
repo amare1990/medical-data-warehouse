a348156d64e8ceffb22a811b71210f8e3076cdc5
amare1990 <amaremek@gmail.com>
2025-02-05 11:29:51 +0300

    Update the crud.py to avoid possible type mismatches by explicitly defining detection_data as DetectionDataSchema

--- fastapi/crud.py
+++ fastapi/crud.py
@@ -2,8 +2,8 @@ from sqlalchemy.orm import Session
 from models import DetectionData
 from schemas import DetectionDataSchema
 
-def create_detection_data(db: Session, detection_data: dict):
-    db_detection = DetectionData(**detection_data)
+def create_detection_data(db: Session, detection_data: DetectionDataSchema):
+    db_detection = DetectionData(**detection_data.dict())  # Convert Pydantic model to dict
     db.add(db_detection)
     db.commit()
     db.refresh(db_detection)
