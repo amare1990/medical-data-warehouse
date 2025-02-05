4793d445303c6f8f8afaa0061b7158fd410474f1
amare1990 <amaremek@gmail.com>
2025-02-05 11:27:45 +0300

    Update schemas.py by adding orm_mode = True in Config inclass to allow Pydantic to work with SQLAlchemy models

--- fastapi/schemas.py
+++ fastapi/schemas.py
@@ -4,3 +4,6 @@ class DetectionDataSchema(BaseModel):
     class_name: str
     confidence: float
     bbox: dict
+
+    class Config:
+        orm_mode = True  # Allows Pydantic to work with SQLAlchemy models
